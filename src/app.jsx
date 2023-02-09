ba48889c82716463bd7e65108d2116082c09b10d
Natalie <natalie.d.hsu@gmail.com>
2023-02-09 01:00:17 -0500

    Connects to DOM

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,45 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import Sketch from "react-p5";
+import {useEffect, useState} from 'preact/hooks'
+import * as d3 from 'd3';
+import {createRef} from "preact";
 
 export function App() {
-    let data;
+    const size = {
+        width: 2000,
+        height: 2000,
+    }
+    const [data, setData] = useState([]);
+    const ref = createRef();
 
-    function setup(p5, canvasParentRef) {
-        p5.createCanvas(800, 800).parent(
-            canvasParentRef
-        );
-        data = p5.loadTable('/data/nft_sales.csv', 'csv', 'header');
+    function renderChart() {
+        d3.csv("/data/digital_art_nfts.csv", n => {
+            setData(data.push(n))
+        });
+
+       var svg =  d3.select(ref.current)
+           .append('svg')
+            .attr("width", size.width)
+            .attr("height", size.height)
+           .style("background-color", "light-grey");
+
+        svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cy", 60)
+            .attr("cx", (d, i) => { return i * 100 + 30; })
+            .attr("r", d => d.price/10);
+
+        console.log(data)
     }
 
-    function draw(p5) {
-        for (let r = 0; r < data.getRowCount(); r++) {
-            for (let c = 0; c < data.getColumnCount(); c++) {
-                console.log(data.getString(r, c))
-            }
-        }
-    }
+    useEffect(() => {
+        renderChart();
+    }, []);
 
-  return (
-    <>
-      <Sketch
-      setup={setup}
-      draw={draw}/>
-    </>
-  )
+    return (
+        <>
+            <div ref={ref}>
+            </div>
+        </>
+    )
 }
