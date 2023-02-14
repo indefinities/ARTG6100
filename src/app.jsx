1a2d53b14ca80137d52af3184c9645f1c398770e
Nat Hsu <natalie.d.hsu@gmail.com>
2023-02-14 11:26:23 -0500

    Dataset added

--- src/app.jsx
+++ src/app.jsx
@@ -1,35 +1,28 @@
-import {useEffect, useState} from 'preact/hooks'
-import * as d3 from 'd3';
 import {createRef} from "preact";
+import {useEffect} from 'preact/hooks'
+import * as d3 from 'd3';
+
 
 export function App() {
     const size = {
-        width: 2000,
-        height: 2000,
+        width: 1000,
+        height: 1000,
     }
-    const [data, setData] = useState([]);
+    const nfts = [];
     const ref = createRef();
 
     function renderChart() {
         d3.csv("/data/digital_art_nfts.csv", n => {
-            setData(data.push(n))
+            nfts.push(n);
         });
 
-       var svg =  d3.select(ref.current)
+
+       const svg =  d3.select(ref.current)
            .append('svg')
             .attr("width", size.width)
             .attr("height", size.height)
-           .style("background-color", "light-grey");
-
-        svg.selectAll("circle")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("cy", 60)
-            .attr("cx", (d, i) => { return i * 100 + 30; })
-            .attr("r", d => d.price/10);
-
-        console.log(data)
+           .data(nfts)
+           .enter();
     }
 
     useEffect(() => {
