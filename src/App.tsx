0b52ee1f4ae3b1edc8a7f1c3178810ed2e4b5c67
Nat Hsu <natalie.d.hsu@gmail.com>
2023-03-30 17:36:21 -0400

    Begin scatterplot for most expensive transactions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Visualization } from "./components/Visualization";
+
 function App() {
 
   return (
     <div className="App">
       <div>
           <h1>NFTS AND THE ETH MARKET</h1>
+          <Visualization />
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Visualization.tsx
+++ src/components/Visualization.tsx
@@ -1,10 +1,27 @@
-export const Visualization = () => {
+import { Scatterplot } from "./Scatterplot";
+import * as d3 from "d3";
+
+export const Visualization = ({ width = 800, height = 800 }) => {
+    let data: {
+        date: Date,
+        eth: number,
+    }[] = [];
+
+    d3.csv('/data/desc_transfers.csv',
+        (t: any) => {
+            return {
+                date: new Date(t.tsdate),
+                eth: t.eth,
+            }
+        })
+        .then((t: any) => {
+            data.push(t);
+        })
+        .catch((error: any) => {
+            console.log(error);
+        });
 
     return (
-        <div className="nft-visualization">
-            <div>
-
-            </div>
-        </div>
-    )
-}
+        <Scatterplot data={data} width={width} height={height} />
+    );
+};
